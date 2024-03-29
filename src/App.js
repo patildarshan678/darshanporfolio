6c081667e252d0aa1846252d9d562c16330d196a
patildarshan678 <patildarshan678@gmail.com>
2024-03-29 22:37:09 +0530

    1.Added React router setup. 2.Created pages compoennt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/home/NavBar';
+import Home from './components/home/Home';
+import Experience from './components/expterties/Experience';
+import Contact from './components/Contact';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="experience" element={ <Experience/> } />
+        <Route path="contact" element={ <Contact/> } />
+      </Routes>
+   
         </div>
   );
 }
--- src/components/home/NavBar.js
+++ src/components/home/NavBar.js
@@ -4,13 +4,34 @@ import Tab from '@mui/material/Tab';
 import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
+import { useNavigate } from "react-router-dom";
 function NavBar() {
     const [value, setValue] = React.useState(0);
     const defaultTabColor= ' #F5F5DC'
+    const navigate = useNavigate();
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+    function tablhandler(index)
+    {
+        switch (index) {
+            case 0:
+                navigate("/")
+              // Code to be executed if expression matches value1
+              break;
+            case 1:
+                navigate("/experience")
+              // Code to be executed if expression matches value2
+              break;
+            // Add more cases as needed
+            case 2:
+                navigate("contact")
+            default:
+              // Code to be executed if expression doesn't match any case
+          }
+    }
     const theme = useTheme();
     const defaultTabStyle ={ backgroundColor: defaultTabColor, color: 'black' }
     return (
@@ -24,10 +45,11 @@ function NavBar() {
                         textColor="inherit"
                         variant="fullWidth"
                         aria-label="full width tabs example"
-                    >
-                       <Tab label="Home" sx={value === 0 ? { color: 'white', backgroundColor: 'black' } : defaultTabStyle} />
-                        <Tab label="Experience" sx={value === 1 ? { color: 'white', backgroundColor: 'black' } :defaultTabStyle} />
-                        <Tab label="Contact" sx={value === 2 ? { color: 'white', backgroundColor: 'black' } : defaultTabStyle} />
+                    >   
+                       <Tab label="Home" sx={value === 0 ? { color: 'white', backgroundColor: 'black' } : defaultTabStyle} onClick={()=>{tablhandler(0)}} />
+                       
+                        <Tab label="Experience" sx={value === 1 ? { color: 'white', backgroundColor: 'black' } :defaultTabStyle} onClick={()=>{tablhandler(1)}}/>
+                        <Tab label="Contact" sx={value === 2 ? { color: 'white', backgroundColor: 'black' } : defaultTabStyle} onClick={()=>{tablhandler(2)}}/>
                     </Tabs>
                 </AppBar>
             </Box>
