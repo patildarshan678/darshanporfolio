1a132a9cfa585728982cfe85243f0f692e87cf9e
patildarshan678 <patildarshan678@gmail.com>
2024-03-30 14:18:26 +0530

    1.Updated the tile

--- src/components/home/HomePage1Components/Homepage1.js
+++ src/components/home/HomePage1Components/Homepage1.js
@@ -1,17 +1,22 @@
 import './Homepage1.css'
 import React from 'react'
+import Typography from '@mui/material/Typography';
 
 import Container from '@mui/material/Container';
 import Cards from './Cards';
+const fontStylesheet ={ fontFamily: 'Roboto' }
 function Homepage1() {
-    const ContainerStyle = {backgroundColor: 'yellow',
-    'min-height': '100vh',
+    const ContainerStyle = {backgroundColor: '',
+    'min-height': '105vh',
 
 }
   return (
     <Container maxWidth="s" style={ContainerStyle}>
-
-        <h2>Key skills</h2>
+<br/>
+<Typography variant="h5"  sx={fontStylesheet}>
+Full Spectrum: A Full Stack Developer's Portfolio
+      </Typography>
+      <br/>
         <Cards/>
     </Container>
   )
